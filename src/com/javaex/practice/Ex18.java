f6c623c6984948287a3c347c9da44e242ac3b128
rlatjgus960 <rlatjgus960@naver.com>
2021-05-31 21:05:36 +0900

    5/31 재연습

--- src/com/javaex/practice/Ex18.java
+++ src/com/javaex/practice/Ex18.java
@@ -12,6 +12,47 @@ public class Ex18 {
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
 		
+		for (int j=num; j>0; j--) {
+			
+			for (int i=0; i<j; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+		}
+		
+		for (int i=1; i<num; i++) {
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}System.out.println("");
+		}
+		
+
+		sc.close();
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 과제 냈던거
+	System.out.print("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		
 		
 		for (int i =num ; i>0; i--) {
 			
@@ -34,13 +75,33 @@ public class Ex18 {
 			System.out.println("");
 
 		}
-		
+ */
 
-		sc.close();
 
-	}
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
